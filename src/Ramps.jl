b0ca986ebba46ea8c58aa4d36f744e48e2444129
tom <tomrottier@hotmail.com>
2022-12-07 16:42:32 +0000

    intial commit

---
+++ src/Ramps.jl
@@ -0,0 +1,67 @@
+module Ramps
+
+export Ramp, MultiRamp, evaluate
+
+struct Ramp
+    x₀::Float64
+    x₁::Float64
+    t₀::Float64
+    t₁::Float64
+end
+
+struct MultiRamp
+    ramps::Vector{Ramp}
+
+    function MultiRamp(ramps::Vector{Ramp})
+        # check suitability of ramps
+        x₀ = ramps[1].x₀
+        t₀ = ramps[1].t₀
+        for (idx, ramp) in enumerate(ramps)
+            # joining ramps must be equal e.g. ramps[1].x₁ == ramps[2].x₀
+            x₀ ≠ ramp.x₀ && error("the start value of ramp $idx ($(ramp.x₀)) == end value of the previous ramp ($x₀)")
+
+            # ramps must ascend e.g. ramps[1].t₁ ≤ ramps[2].t₀
+            ramp.t₀ < t₀ && error("the t₀ of ramp $idx ($(ramp.t₀)) must be ≥ t₁ of previous ramp ($t₀)")
+
+            x₀ = ramp.x₁
+            t₀ = ramp.t₁
+        end
+
+        return new(ramps)
+    end
+end
+
+## polynomial with desired properties
+f(x) = x^3 * (6x^2 - 15x + 10)
+fp(x) = x^2 * (30x^2 - 60x + 30)
+fpp(x) = x * (120x^2 - 180x + 60)
+
+
+## ramp function scaled to domain and range
+function evaluate(x₀, x₁, t₀, t₁, t, n=0)
+    # scale domain
+    x = (t - t₀) / (t₁ - t₀)
+
+    # choose derivative
+    n == 0 ? g = f : n == 1 ? g = fp : n == 2 ? g = fpp : error("higher derivatives not implemented yet")
+
+    return t < t₀ ? x₀ : t₀ ≤ t ≤ t₁ ? x₀ + (x₁ - x₀) * g(x) : t > t₁ ? x₁ : error("t = $t not a number?")
+end
+
+evaluate(r::Ramp, t, n=0) = evaluate(r.x₀, r.x₁, r.t₀, r.t₁, t, n)
+
+function evaluate(r::MultiRamp, t, n=0)
+    x₀ = r.ramps[1].x₀
+    t₀ = r.ramps[1].t₀
+    t < t₀ && return x₀
+
+    for ramp in r.ramps
+        t ≤ ramp.t₁ && return evaluate(ramp, t, n)
+        x₀ = ramp.x₁
+        t₀ = ramp.t₁
+    end
+
+    return r.ramps[end].x₁
+end
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,37 @@
+using Test, Ramps
+
+@testset "all" verbose = true begin
+    @testset "polynomial" verbose = true begin
+        @test Ramps.f(0) == 0
+        @test Ramps.f(1) == 1
+        @test Ramps.fp(0) == 0
+        @test Ramps.fp(1) == 0
+        @test Ramps.fpp(0) == 0
+        @test Ramps.fpp(1) == 0
+    end
+
+    @testset "single ramp" verbose = true begin
+        r = Ramp(0.0, 2.0, 0.0, 1.5)
+        @test evaluate(r, -0.5) == 0.0
+        @test evaluate(r, 1.5) == 2.0
+        @test evaluate(r, 0.0, 1) == 0.0
+        @test evaluate(r, 1.5, 1) == 0.0
+        @test evaluate(r, 0.0, 2) == 0.0
+        @test evaluate(r, 1.5, 2) == 0.0
+
+    end
+
+    @testset "multi ramps" verbose = true begin
+        r1 = Ramp(0.0, 0.8, 0.2, 0.5)
+        r2 = Ramp(0.8, 0.6, 0.8, 1.2)
+        r = MultiRamp([r1, r2])
+
+        @test evaluate(r, 0.0) == 0.0
+        @test evaluate(r, 0.5) == 0.8
+        @test evaluate(r, 0.6) == 0.8
+        @test evaluate(r, 1.2) == 0.6
+        @test evaluate(r, 1.5) == 0.6
+    end
+    ≈
+
+end
